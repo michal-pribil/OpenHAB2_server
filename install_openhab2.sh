e384294552a55b0114de300afd88ad10a6ac38f6
Michal Pribil <pribil.michal@gmail.com>
2020-03-28 15:14:42 +0100

    Added script to setup OpenHab in the target folder and install it

--- install_openhab2.sh
+++ install_openhab2.sh
@@ -1,8 +1,12 @@
+git clone https://github.com/michal-pribil/OpenHAB2.git /etc/openhab2
+
 wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add -
 apt-get install apt-transport-https
 echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list
 apt-get update
 sudo apt-get install openhab2
 /bin/systemctl start openhab2.service
+
+
 
 
